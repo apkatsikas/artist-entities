549b6f7de14afbf4b824a618d66dcb93c2e894c7
andrew-katsikas <andrew.katsikas@vimeo.com>
2025-10-15 19:28:48 -0400

    Remove commented out test

--- models/ArtistModel_test.go
+++ models/ArtistModel_test.go
@@ -76,27 +76,3 @@ func TestArtistInvalidData(t *testing.T) {
 		})
 	}
 }
-
-// func TestRandomOffset(t *testing.T) {
-// 	count := uint(666)
-// 	result := rules.RandomOffset(count)
-
-// 	withinBounds := result >= 1 && result <= count
-// 	assert.True(t, withinBounds)
-// }
-
-// func TestRandomCount1(t *testing.T) {
-// 	count := uint(1)
-// 	rules := ArtistRules{}
-
-// 	result := rules.RandomOffset(count)
-
-// 	assert.Equal(t, count, result)
-// }
-
-// func TestRandomOffsetPanics(t *testing.T) {
-// 	count := uint(0)
-// 	rules := ArtistRules{}
-
-// 	assert.Panics(t, func() { rules.RandomOffset(count) })
-// }
